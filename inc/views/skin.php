229475b1a9fb80ef91ec5961c621d6b9a173b6df
gn_themes <gn_themes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-21 03:58:17 +0000

    4.1.0
    git-svn-id: https://plugins.svn.wordpress.org/shortcodes-ultimate/tags/4.1.0@776245 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/views/skin.php
@@ -0,0 +1,22 @@
+<?php
+	$upload_dir = wp_upload_dir();
+	$kins_path = trailingslashit( path_join( $upload_dir['basedir'], 'shortcodes-ultimate-skins' ) );
+	$option['options'] = array( 'default' );
+	foreach ( glob( $kins_path . '*', GLOB_ONLYDIR ) as $dir ) $option['options'][] = str_replace( $kins_path, '', $dir );
+?>
+<tr>
+	<th scope="row">
+		<label for="sunrise-plugin-field-<?php echo $option['id']; ?>"><?php echo $option['name']; ?></label></th>
+	<td>
+		<select name="<?php echo $option['id']; ?>" id="sunrise-plugin-field-<?php echo $option['id']; ?>" class="sunrise-plugin-select">
+			<?php
+				foreach ( $option['options'] as $skin ) {
+					$selected = ( $settings[$option['id']] == $skin ) ? ' selected="selected"' : '';
+					?>
+					<option value="<?php echo $skin; ?>"<?php echo $selected; ?>><?php echo $skin; ?></option>
+				<?php
+				}
+			?>
+		</select> <span class="description"><?php echo $option['desc']; ?></span>
+	</td>
+</tr>
